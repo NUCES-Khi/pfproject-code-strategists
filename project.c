a80964b410a8b89d04977501df3810ccb49eba5e
amnaasim24 <142867835+amnaasim24@users.noreply.github.com>
2023-12-21 12:41:49 +0500

    Update project.c

--- project.c
+++ project.c
@@ -150,3 +150,82 @@ void displayPassengerDetails(struct Passenger passenger) {
     printf("Seat Number: %d\n", passenger.seatNumber);
     printf("Train Number: %d\n", passenger.trainNumber);
 }
+void displayAllPassengers(struct Passenger passengers[], int numPassengers) {
+    printf("\nAll Passengers:\n");
+    for (int i = 0; i < numPassengers; i++) {
+        displayPassengerDetails(passengers[i]);
+        printf("\n");
+    }
+}
+void saveDataToFile(struct Train trains[], int numTrains, struct Passenger passengers[], int numPassengers) {
+    // Implement saving data to files
+    FILE* trainsFilePtr = fopen("trains.txt", "w");
+    FILE* passengersFilePtr = fopen("passengers.txt", "w");
+
+    if (trainsFilePtr == NULL || passengersFilePtr == NULL) {
+        printf("Error opening files for writing.\n");
+        return;
+    }
+
+    // Save train data
+    for (int i = 0; i < numTrains; i++) {
+     fprintf(trainsFilePtr, "%d,%s,%s", trains[i].trainNumber, trains[i].name, trains[i].destination);
+      for (int j = 0; j < MAX_CARRIAGES; j++) {
+        fprintf(trainsFilePtr, ",%d", trains[i].carriages[j].availableSeats);
+      }
+     fprintf(trainsFilePtr, "\n");
+}
+
+    fclose(trainsFilePtr);
+
+    // Save passenger data
+    for (int i = 0; i < numPassengers; i++) {
+        fprintf(passengersFilePtr, "%s,%d,%d,%d\n", passengers[i].name, passengers[i].age, passengers[i].seatNumber, passengers[i].trainNumber);
+    }
+
+    fclose(passengersFilePtr);
+
+    printf("Data saved successfully.\n");
+}
+
+void loadDataFromFile(struct Train trains[], int* numTrains, struct Passenger passengers[], int* numPassengers) {
+    // Implement loading data from files
+    FILE* trainsFilePtr = fopen("trains.txt", "r");
+    FILE* passengersFilePtr = fopen("passengers.txt", "r");
+
+    if (trainsFilePtr == NULL || passengersFilePtr == NULL) {
+        printf("Error opening files for reading.\n");
+        return;
+    }
+
+ // Load train data
+ // Adjust the number of format specifiers to match the actual number of items
+    while (fscanf(trainsFilePtr, "%d,%49[^,],%49[^,]",&trains[*numTrains].trainNumber, trains[*numTrains].name, trains[*numTrains].destination) == 3) {
+        int carriageIndex = 0;
+        int seats;
+        // Read available seats for each carriage dynamically until the end of the line
+        while (fscanf(trainsFilePtr, ",%d", &seats) == 1) {
+            if (carriageIndex < MAX_CARRIAGES) {
+                trains[*numTrains].carriages[carriageIndex].availableSeats = seats;
+                carriageIndex++;
+            } else {
+                printf("Error: Too many carriages for a train.\n");
+                break;
+            }
+        }
+        if (*numTrains >= MAX_TRAINS) {
+            printf("Error: Too many trains read from the file.\n");
+            break;
+        }
+
+        (*numTrains)++;
+    }
+    fclose(trainsFilePtr);
+    // Load passenger data
+    while (fscanf(passengersFilePtr, "%49[^,],%d,%d,%d\n", passengers[*numPassengers].name, &passengers[*numPassengers].age,
+                  &passengers[*numPassengers].seatNumber, &passengers[*numPassengers].trainNumber) == 4) {
+        (*numPassengers)++;
+    }
+    fclose(passengersFilePtr);
+    printf("Data loaded successfully.\n");
+}
